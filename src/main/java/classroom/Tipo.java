578d6b20e27eae466aca0dd0e207ad7385b2e049
OJBatex <simondr2006@gmail.com>
2024-03-02 22:01:55 -0500

    Intento 2

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -4,9 +4,15 @@ public enum Tipo {
 	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
 	
 	private int codigo;
-	private String nombre;
-	private Tipo(int codigo,String nombre) {
+	private Tipo(int codigo) {
+		this.setCodigo(codigo);
+		
+	
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
 		this.codigo = codigo;
-		this.nombre = nombre;
 	}
 }
